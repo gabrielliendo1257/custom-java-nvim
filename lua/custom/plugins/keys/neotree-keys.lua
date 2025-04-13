7e1dd216ccef18977760c7945856fd2820bc9aaa
gabriel <gabrielliendo1257@gmail.com>
2025-04-13 13:00:57 -0500

    add <cr> on Neotree command-keys

--- lua/custom/plugins/keys/neotree-keys.lua
+++ lua/custom/plugins/keys/neotree-keys.lua
@@ -1,4 +1,4 @@
 return {
 
-  { '<leader>e', '<cmd>Neotree', desc = '[O]pen Neotree', mode = 'n', silent = true },
+  { '<leader>e', '<cmd>Neotree<cr>', desc = '[O]pen Neotree', mode = 'n', silent = true },
 }

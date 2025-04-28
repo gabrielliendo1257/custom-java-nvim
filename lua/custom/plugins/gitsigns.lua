252649ec552a1eccba8902d182a8cf1120e4716e
gabriel <gabrielliendo1257@gmail.com>
2025-04-28 10:52:12 -0500

    gitsigns refactor

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,12 +1,5 @@
-return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+return {
+    'lewis6991/gitsigns.nvim',
+    keys = require("custom.plugins.keys.gitsigns-keys"),
+    config = true
 }
---
+++ lua/custom/plugins/keys/gitsigns-keys.lua
@@ -0,0 +1,5 @@
+return {
+    { '<leader>gtc', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = '[G]it [T]oggle current inline', mode = 'n', silent = true },
+    { '<leader>gtd', '<cmd>Gitsigns toggle_deleted<cr>', desc = '[G]it [T]oggle deleted', mode = 'n', silent = true },
+    { '<leader>gsb', '<cmd>Gitsigns stage_buffer<cr>', desc = '[G]it [S]tage buffer', mode = 'n', silent = true },
+}
